c890ce90d46f015d95a757170715abe764214d35
Nastenika777 <egorovaanastasia733@gmail.com>
2022-11-17 17:59:35 +0300

    Third task

--- Seminar1_Homework.cs
+++ Seminar1_Homework.cs
@@ -13,18 +13,29 @@
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Введите число а:");
-int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число а:");
+//int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число b:");
-int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число b:");
+//int num2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число c:");
-int num3 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите число c:");
+//int num3 = Convert.ToInt32(Console.ReadLine());
 
-int max=num1;
+//int max=num1;
 
-if(num2 > max) max = num2;
-if(num3 > max) max = num3;
+//if(num2 > max) max = num2;
+//if(num3 > max) max = num3;
 
-Console.WriteLine($"a = {num1}, b = {num2}, c = {num3} -> max = {max}");
+//Console.WriteLine($"a = {num1}, b = {num2}, c = {num3} -> max = {max}");
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+if(Int32.Parse(num)%2==0){
+    Console.WriteLine("Число чётное");
+}
+else{
+    Console.WriteLine("Число нечётное");
+}
